3bfa1b13d40f374f92c821fa8326a74d7aec829a
duy270101 <91267863+duy270101@users.noreply.github.com>
2021-09-23 21:34:34 +0700

    duy

---
+++ bai4.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<meta charset='utf-8'>";
+    echo "<h3> bài 4 </h3>"."<br>";
+    echo "Sử dụng ngôn ngữ PHP hiển thị một số tự nhiên x được phát sinh ngẫu nhiên; một số tự nhiên
+    \$y được phát sinh ngẫu nhiên với 50<\$y<100."."<br>";
+    $a = rand();
+    $b = rand (50,100);
+    echo " hai số ngẫu nhiên là \$a vơi \$b là: $a, $b"
+?>
